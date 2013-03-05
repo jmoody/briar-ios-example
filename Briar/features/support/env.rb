96537ffd2a36f67b07a0d0cc124185ed7b72b89e
Joshua Moody <joshuajmoody@gmail.com>
2013-03-05 12:30:48 +0100

    changing up ruby mine settings. gemfile now says use briar 0.0.3.  support now uses the World function to include _all_ briar modules.  attempts to fix the lesspainful problems

--- Briar/features/support/env.rb
+++ Briar/features/support/env.rb
@@ -1,2 +1,21 @@
 require 'calabash-cucumber/cucumber'
 require 'briar'
+
+World(Briar)
+World(Briar::Core)
+World(Briar::Alerts_and_Sheets)
+World(Briar::Bars)
+World(Briar::Control::Button)
+World(Briar::Control::Segmented_Control)
+World(Briar::Control::Slider)
+World(Briar::Picker::Date)
+World(Briar::Picker_Shared)
+World(Briar::Picker)
+World(Briar::Email)
+World(Briar::ImageView)
+World(Briar::Keyboard)
+World(Briar::Label)
+World(Briar::ScrollView)
+World(Briar::Table)
+World(Briar::TextField)
+World(Briar::TextView)
