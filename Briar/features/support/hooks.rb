515bebd41b177e9768b317c864012a071c1e9a40
Joshua Moody <joshuajmoody@gmail.com>
2013-04-06 15:19:52 +0200

    updated code signing (why they had changed is a mystery) and added EMBED variable to hooks and cucumber profiles to control whether or not screenshots are embedded

--- Briar/features/support/hooks.rb
+++ Briar/features/support/hooks.rb
@@ -9,7 +9,9 @@ end
 
 #noinspection RubyUnusedLocalVariable
 After do |scenario|
-  res = http({:method => :get, :path => 'screenshot'})
-  image_data = Base64.encode64(res).gsub("\n", "")
-  embed("data:image/png;base64,#{image_data}", 'image/png')
+  if ENV['EMBED']
+    res = http({:method => :get, :path => 'screenshot'})
+    image_data = Base64.encode64(res).gsub("\n", "")
+    embed("data:image/png;base64,#{image_data}", 'image/png')
+  end
 end
