0c7e58da8ab166c887521200ad83733cef195f2b
Joshua Moody <joshuajmoody@gmail.com>
2013-10-15 12:08:03 +0200

    preparing for slider feature * added venus iOS device * wrestling with ruby mine run configurations (EAP really messed   things up

---
+++ Briar/features/calabash_issues/fb_164_sliders.feature
@@ -0,0 +1,6 @@
+Feature:
+  In order to <meet some goal>
+  As a <type of stakeholder>
+  I want <a feature>
+
+  Scenario:
---
+++ Briar/venus_irb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+bundle update
+bundle install
+rbenv rehash
+
+# send a killall to instruments
+# tried all manner of output redirects like:
+#
+# killall -9 instruments >/dev/null 2>&1`
+#
+# but instruments continued to spam the shell
+#INSTRUMENTS_SPAM=`killall -9 instruments >/dev/null 2>&1`
+
+IP=`cat ~/.xamarin/devices/venus/ip`
+UDID=`cat ~/.xamarin/devices/venus/udid`
+PLAYBACK_DIR="features/playback" \
+CALABASH_FULL_CONSOLE_OUTPUT=0 \
+DEVICE=ipad \
+DEBUG=0 \
+DEVICE_TARGET=${UDID} \
+BUNDLE_ID="com.littlejoysoftware.Briar-cal" \
+DEVICE_ENDPOINT=${IP} \
+IRBRC=.irbrc \
+bundle exec irb
+
+
+
