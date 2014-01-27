4d6fe841e0937496e682d8fb1f13edc964ab8ade
Joshua Moody <joshuajmoody@gmail.com>
2014-01-27 14:03:09 +0100

    testing assert_map_results

---
+++ Briar/features/calabash_issues/trello_631_expect_map_results.feature
@@ -0,0 +1,113 @@
+@trello
+@trello_631
+Feature: expecting map results
+  In order to reduce duplicate code
+  As a calabash developer
+  I want a nice way to assert that +map+ returns valid results
+
+  @text
+  @deprecated_function
+  Scenario: expecting set_text to pass
+    Given I am looking at the Text tab
+    Then I should be able to set the text in the top text field to "foobar"
+
+  @allow-rescue
+  @text
+  @deprecated_function
+  Scenario: expecting set_text to fail
+    Then I should get an exception using set_text on a text field that does not exist
+
+  @text
+  @keyboard
+  Scenario: expecting clear_text to pass on text fields
+    Given I am looking at the Text tab
+    And I type "foobar" into the top text field
+    Then I should be able to clear the top text field with clear_text
+
+  @allow-rescue
+  @text
+  @keyboard
+  Scenario: expecting clear_text to fail on text fields
+    Then I should get an exception using clear_text on a text field that does not exist
+
+  @text
+  @keyboard
+  Scenario: expecting clear_text to pass on text views
+    Given I am looking at the Text tab
+    And I type "foobar" into the top text view
+    Then I should be able to clear the top text view with clear_text
+
+  @allow-rescue
+  @text
+  @keyboard
+  Scenario: expecting clear_text to fail on a text view
+    Then I should get an exception using clear_text on a text view that does not exist
+
+  @allow-rescue
+  @text
+  @keyboard
+  Scenario: expecting clear_text to fail because the view does not respond to setText
+    Given I am looking at the Text tab
+    Then I should get an exception using clear_text on a view that does not respond to setText
+
+  @date_picker
+  Scenario:  expecting to be able to use picker_set_date_time
+    Given that I am looking at the Date tab
+    And I am looking at the date and time picker
+    Then I change the date picker date to "Sep 14 2013" at "13:15"
+
+  @allow-rescue
+  @date_picker
+  Scenario:  expecting that picker_set_date_time will raise an error if no view is found
+    Then I call picker_set_date_time when there is no date picker visible
+
+  @allow-rescue
+  @date_picker
+  Scenario:  expecting that picker_set_date_time will raise an error if called with a non-DateTime object
+    Then I call picker_set_date_time with a non-DateTime object
+
+  @table
+  @scroll
+  Scenario: expecting scroll to pass
+    Given I am looking at the Tables tab
+    Then I call scroll down on the table
+
+  @allow-rescue
+  @scroll
+  Scenario: expecting scroll to fail because it cannot find a view
+    Then I call scroll on a non-existent view
+
+  # smoke tested on Rise Up because Briar does not have any UIWebViews
+  #todo make a test for scroll fail on non webView
+  @allow-rescue
+  @scroll
+  Scenario: expecting scroll to fail because the view is not a scrollView
+    Then I call scroll on a view that is not a scroll view
+
+  # scroll_to_row and scroll_to_cell call the same map function: scrollToRow
+  # so there is no need to test scroll_to_cell in this context
+  @scroll
+  @table
+  Scenario: expecting scroll_to_row to pass
+    Given I am looking at the Tables tab
+    Then I call scroll_to_row 10
+
+  @scroll
+  @table
+  @allow-rescue
+  Scenario: expecting scroll_to_row to fail because the query returned a non-table view
+    Then I call scroll_to_row on a non-existent view
+
+  @scroll
+  @table
+  Scenario: expecting scroll_to_row_with_mark to pass
+    Given I am looking at the Tables tab
+    Then I call scroll_to_row_with_mark on the "j" row
+
+  @scroll
+  @table
+  @allow-rescue
+  Scenario: expecting scroll_to_row_with_mark to fail because the query returned a non-table view
+    Then I call scroll_to_row_with_mark on a non-existent view
+
+
---
+++ Briar/features/step_definitions/calabash_issues/trello_631_expect_map_results_steps.rb
@@ -0,0 +1,120 @@
+
+Then(/^I should be able to set the text in the top text field to "([^"]*)"$/) do |arg|
+  set_text("textField marked:'top tf'", arg)
+end
+
+Then(/^I should get an exception using set_text on a text field that does not exist$/) do
+  begin
+    set_text("textField marked:'bad mark'", 'hey!')
+    error_no_rescue 'expected set_text to fail because it could not find the marked view'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+And(/^I type "([^"]*)" into the (top|bottom) text (field|view)$/) do |arg, loc, clz|
+  qstr = "text#{clz.capitalize} marked:'#{loc} #{clz == 'field' ? 'tf' : 'tv'}'"
+  touch(qstr)
+  wait_for_keyboard
+  ensure_text_input(arg, 'typing')
+end
+
+Then(/^I should be able to clear the (top|bottom) text (field|view) with clear_text$/) do |loc,clz|
+  qstr = "text#{clz.capitalize} marked:'#{loc} #{clz == 'field' ? 'tf' : 'tv'}'"
+  clear_text(qstr)
+  res = query(qstr, :text).first
+  unless res == ''
+    screenshot_and_raise "expected top text field to have '' but found '#{res}'"
+  end
+end
+
+Then(/^I should get an exception using clear_text on a text (field|view) that does not exist$/) do |clz|
+  begin
+    clear_text("text#{clz.capitalize} marked:'bad mark'", 'hey !')
+    error_no_rescue 'expected clear_text to fail because it could not find the marked view'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+Then(/^I should get an exception using clear_text on a view that does not respond to setText$/) do
+  begin
+    clear_text("button marked:'the button'")
+    error_no_rescue "expected clear_text to fail because query found a view that did not respond to 'setText'"
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+And(/^I am looking at the date and time picker$/) do
+  touch_button_and_wait_for_view('show date and time picker','date and time picker')
+end
+
+Then(/^I call picker_set_date_time when there is no date picker visible$/) do
+  begin
+    picker_set_date_time(DateTime.now)
+    error_no_rescue 'expected picker_set_date_time to fail because query found no date picker'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+
+Then(/^I call picker_set_date_time with a non-DateTime object$/) do
+  begin
+    picker_set_date_time(Time.now)
+    error_no_rescue 'expected picker_set_date_time to fail because it was passed a non-DateTime object'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+Then(/^I call scroll down on the table$/) do
+  table_mark = 'alphabet'
+  scroll("view marked:'#{table_mark}'", :down)
+end
+
+Then(/^I call scroll on a non-existent view$/) do
+  begin
+    scroll("view marked:'no mark!'", :down)
+    error_no_rescue 'expected scroll to fail because query found on view to scroll on'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+
+Then(/^I call scroll on a view that is not a scroll view$/) do
+  begin
+    scroll("view marked:'buttons'", :down)
+    error_no_rescue 'expected scroll to fail because the query returned an non-scroll view'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+Then(/^I call scroll_to_row 10$/) do
+  scroll_to_row("view marked:'alphabet'", 10)
+end
+
+Then(/^I call scroll_to_row on a non-existent view$/) do
+  begin
+    scroll_to_row("view marked:'buttons'", 1)
+    error_no_rescue 'expected scroll_to_row to fail because the query returned an non-table view'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+Then(/^I call scroll_to_row_with_mark on a non-existent view$/) do
+  begin
+    scroll_to_row_with_mark('m')
+    error_no_rescue 'expected scroll_to_row_with_mark to fail because the query returned an non-table view'
+  rescue
+    puts 'correctly threw exception' if full_console_logging?
+  end
+end
+
+Then(/^I call scroll_to_row_with_mark on the "([^"]*)" row$/) do |arg|
+  scroll_to_row_with_mark arg
+end
