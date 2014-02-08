bf141af810eb182fd481fc801f6cd5ab2f56e9c8
Joshua Moody <joshuajmoody@gmail.com>
2014-02-08 03:16:05 +0100

    installed latest the server version 0.9.167.pre1 - no changes relevant to the return key issue

---
+++ Briar/features/calabash_issues/issue_309_keyboard_return.feature
@@ -0,0 +1,15 @@
+@issue_309
+@keyboard
+@xtc_wip
+Feature:  touching the keyboard action key AKA 'Return' AKA done
+  In order not break keyboard interaction
+  As a calabash framework tester
+  I want a way to ensure I can touch the keyboard action key
+
+  Background: get me to the text related view
+    Given I am looking at the Text tab
+    Given I choose a text input view at random
+    And that text input view has the default keyboard
+
+  Scenario: i want to touch the return key on the text input view
+    Then I should be able to touch the Return key
---
+++ Briar/features/helpers/briar_keyboard_helpers.rb
@@ -0,0 +1,102 @@
+module Briar
+  module Helpers
+    module Keyboard
+
+
+      #UIKeyboardTypeDefault,
+      #UIKeyboardTypeASCIICapable,
+      #UIKeyboardTypeNumbersAndPunctuation,
+      #UIKeyboardTypeURL,
+      #UIKeyboardTypeNumberPad,
+      #UIKeyboardTypePhonePad,
+      #UIKeyboardTypeNamePhonePad,
+      #UIKeyboardTypeEmailAddress,
+      #UIKeyboardTypeDecimalPad,
+      #UIKeyboardTypeTwitter,
+      #UIKeyboardTypeWebSearch,
+
+      UI_KEYBOARD_TYPE =
+            {0 => :default,
+             1 => :ascii_capable,
+             2 => :numbers_and_punctuation,
+             3 => :url,
+             4 => :number_pad,
+             5 => :phone_pad,
+             6 => :name_phone_pad,
+             7 => :email,
+             8 => :decimal,
+             9 => :twitter,
+             10 => :web_search}
+
+
+      def canonical_keyboard_type(ui_keyboard_type)
+        if ui_keyboard_type.is_a?(Fixnum)
+          UI_KEYBOARD_TYPE[ui_keyboard_type]
+        else
+          raise "expected '#{ui_keyboard_type}' to be a Fixnum"
+        end
+      end
+
+      def ui_keyboard_type(canonical_keyboard_type)
+        if canonical_keyboard_type.is_a?(Symbol)
+          Hash[UI_KEYBOARD_TYPE.map(&:reverse)][canonical_keyboard_type]
+        else
+          raise "expected '#{canonical_keyboard_type}' to be a Symbol"
+        end
+      end
+
+
+      def keyboard_type_with_query(query_str, opts={})
+        default_opts = {:timeout_message => "after waiting '#{query_str}' did not find a match"}
+        opts = default_opts.merge(opts)
+
+        res = nil
+
+        wait_for(opts) do
+          res = query(query_str, :keyboardType)
+          not res.nil?
+        end
+
+        unless res.count == 1
+          screenshot_and_raise "expected exactly one element to be returned by '#{query_str}' but found '#{res}'"
+        end
+
+        canonical_keyboard_type(res.first)
+      end
+
+      def _kb_type_with_step_arg(arg)
+        case arg
+          when 'ascii' then
+            target = :ascii_capable
+          when 'number' then
+            target = :number_pad
+          when 'phone' then
+            target = :phone_pad
+          when 'name and phone' then
+            target = :name_phone_pad
+          else
+            target = "#{arg.gsub(' ', '_')}".to_sym
+        end
+        target
+      end
+
+
+      def ensure_keyboard_type(query_str, type, opts={})
+        target_ui_type = ui_keyboard_type(type)
+        if target_ui_type.nil?
+          raise "expected '#{type}' to be a valid canonical type for '#{UI_KEYBOARD_TYPE}'"
+        end
+
+        current = keyboard_type_with_query(query_str, opts)
+        unless current.eql?(type)
+          res = query("#{query_str}", {:setKeyboardType => target_ui_type})
+          if res.empty?
+            screenshot_and_raise "could not set keyboard type for '#{query_str}' to '#{target_ui_type}' found '#{res}'"
+          end
+        end
+      end
+    end
+  end
+end
+
+World(Briar::Helpers::Keyboard)
