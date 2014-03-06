b5cf257d7c05ee7714576c5db02d1e5738acbf93
Joshua Moody <joshuajmoody@gmail.com>
2014-03-06 17:30:11 +0100

    moved keychain steps file

---
+++ Briar/features/step_definitions/calabash_issues/issue_323_keychain_steps.rb
@@ -0,0 +1,14 @@
+Unstaged changes after reset:
+M	Briar/Briar/SSKeychainQuery.h
+M	Briar/Briar/SSKeychainQuery.m
+M	Briar/Gemfile
+M	Briar/calabash.framework/Versions/A/calabash
+M	Briar/calabash.framework/Versions/Current/calabash
+M	Briar/calabash.framework/calabash
+M	Briar/config/xtc-profiles.yml
+M	Briar/features/calabash_issues/issue_323_keychain.feature
+M	Briar/features/calabash_issues/trello_631_expect_map_results.feature
+M	Briar/features/step_definitions/calabash_issues/issue_323_keychain_steps.rb
+M	Briar/features/step_definitions/calabash_issues/trello_631_expect_map_results_steps.rb
+D	Briar/features/step_definitions/keychain_steps.rb
+M	Briar/features/step_definitions/text_related.rb
