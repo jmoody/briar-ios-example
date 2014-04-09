ffb188aa8a93aa06487421551213b271fb859620
Joshua Moody <joshuajmoody@gmail.com>
2014-04-09 18:09:40 +0200

    bumped calabash.framework version

---
+++ Briar/calabash.framework/Versions/A/Headers/LPVersionRoute.h
@@ -0,0 +1,16 @@
+//
+//  LPVersionRoute.h
+//  calabash
+//
+//  Created by Karl Krukow on 22/06/12.
+//  Copyright (c) 2012 LessPainful. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LPRoute.h"
+
+#define kLPCALABASHVERSION @"CALABASH VERSION: 0.9.169.pre"
+
+@interface LPVersionRoute : NSObject <LPRoute>
+
+@end
