cc36931f2c453a98f309de5f7d391531bbab6a23
Joshua Moody <joshuajmoody@gmail.com>
2014-05-07 11:32:31 +0200

    replacing xamarin-build with xtc-prepare.sh

---
+++ Briar/xtc-prepare.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+XAMARIN_DIR="${PWD}/xtc-staging"
+
+echo "INFO: creating the ./xtc-staging directory"
+rm -rf "${XAMARIN_DIR}"
+mkdir -p "${XAMARIN_DIR}"
+
+echo "INFO: copying features over to ${XAMARIN_DIR}"
+cp -r features "${XAMARIN_DIR}/"
+
+echo "INFO: installing cucumber.yml to ${XAMARIN_DIR}"
+cp "./config/xtc-profiles.yml" "${XAMARIN_DIR}/cucumber.yml"
+
+echo "INFO: installing briar predefined steps to .xamarin/features"
+echo "INFO:   - see briar/cucumber.rb for details"
+BRIAR_INSTALL=`bundle show briar`
+BRIAR_STEPS="${BRIAR_INSTALL}/features/step_definitions"
+cp -r "${BRIAR_STEPS}" "${XAMARIN_DIR}/features/step_definitions/briar"
+
+WORKSPACE="../briar-ios-example.xcworkspace"
+SCHEME="Briar-cal"
+TARGET_NAME="Briar-cal"
+CONFIG="Debug"
+
+CAL_DISTRO_DIR="build/calabash"
+ARCHIVE_BUNDLE="${CAL_DISTRO_DIR}/briar-cal.xcarchive"
+APP_BUNDLE_PATH="${ARCHIVE_BUNDLE}/Products/Applications/Briar-cal.app"
+IPA_PATH="${CAL_DISTRO_DIR}/${TARGET_NAME}.ipa"
+
+rm -rf "${CAL_DISTRO_DIR}"
+mkdir -p "${CAL_DISTRO_DIR}"
+
+set +o errexit
+
+xcodebuild archive -workspace "${WORKSPACE}" -scheme "${SCHEME}" \
+    -configuration "${CONFIG}" -archivePath "${ARCHIVE_BUNDLE}" \
+    -sdk iphoneos | bundle exec xcpretty -c
+
+RETVAL=${PIPESTATUS[0]}
+
+set -o errexit
+
+if [ $RETVAL != 0 ]; then
+    echo "FAIL:  archive failed"
+    exit $RETVAL
+fi
+
+set +o errexit
+
+xcrun -sdk iphoneos PackageApplication -v "${PWD}/${APP_BUNDLE_PATH}" -o "${PWD}/${IPA_PATH}" > /dev/null
+
+RETVAL=$?
+
+set -o errexit
+
+if [ $RETVAL != 0 ]; then
+    echo "FAIL:  export archive failed"
+    exit $RETVAL
+fi
+
+cp "${IPA_PATH}" "${XAMARIN_DIR}/"
