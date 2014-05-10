7e2e327bb2af78124c6d76c6b7c454ab6a01d262
Joshua Moody <joshuajmoody@gmail.com>
2014-05-10 03:32:43 +0200

    should have an xtc build script

---
+++ Briar/jenkins-xtc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ "$USER" = "jenkins" ]; then
+    echo "INFO: hey, you are jenkins!  loading ~/.bash_profile_ci"
+    source ~/.bash_profile_ci
+    hash -r
+    rbenv rehash
+
+    rm -f .env
+    cp .env-jenkins .env
+
+else
+   echo "INFO: you are _not_ jenkins! will not load ~/.bash_profile_ci"
+fi
+
+rbenv exec bundle install
+
+rbenv exec bundle exec briar xtc jenkins-ci jenkins
