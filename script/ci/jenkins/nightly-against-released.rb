47c09b76380febf452852fdfd53bae94d62fe045
Joshua Moody <joshuajmoody@gmail.com>
2014-08-08 06:53:08 +0200

    common tasks should be in ci-prepare.rb

--- script/ci/jenkins/nightly-against-released.rb
+++ script/ci/jenkins/nightly-against-released.rb
@@ -7,21 +7,11 @@ xtc_profile = ARGV[1] || 'default'
 
 ### prepare
 
-update_rubygems
-uninstall_gem 'briar'
-uninstall_gem 'calabash-cucumber'
-uninstall_gem 'run_loop'
-uninstall_gem 'xamarin-test-cloud'
+do_system(File.expand_path(File.join(File.dirname(__FILE__), 'ci-prepare.rb')))
 
 working_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'Briar'))
 
 Dir.chdir working_dir do
-
-  # maybe wrap this up?
-  do_system('rm -rf Gemfile.lock')
-  do_system('rm -rf .bundle')
-  do_system('rm -rf vendor')
-  do_system('bundle install')
 
   do_system('rm -rf calabash.framework')
   do_system('bundle exec calabash-ios download')
