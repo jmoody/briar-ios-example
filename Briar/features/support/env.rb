e9f0dad6b939d9ce2e8a219661bf42c5f732960c
Joshua Moody <joshuajmoody@gmail.com>
2014-08-08 06:49:16 +0200

    monkey-patch cucumber 'pending' for XTC
    Pending tests fail but generate no reports.
    
    https://github.com/xamarin/test-cloud-frontend/issues/186

--- Briar/features/support/env.rb
+++ Briar/features/support/env.rb
@@ -9,3 +9,15 @@ if ENV['XAMARIN_TEST_CLOUD'] != '1'
 end
 
 I18n.enforce_available_locales = false
+
+# override cucumber pending on the XTC
+if ENV['XAMARIN_TEST_CLOUD'] == '1'
+  module Cucumber
+    module RbSupport
+      def pending(message = 'TODO')
+        raise "PENDING: #{message}"
+      end
+    end
+  end
+  World(Cucumber::RbSupport)
+end
