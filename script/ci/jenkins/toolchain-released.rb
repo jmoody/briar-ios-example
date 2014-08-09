50b3ca3c8a69b2943f6316211a84471ba8322c1e
Joshua Moody <joshuajmoody@gmail.com>
2014-08-10 01:27:42 +0200

    script to run against the released toochain

---
+++ script/ci/jenkins/toolchain-released.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), 'ci-helpers'))
+
+xtc_device_set = ARGV[0] || 'toolchain-released'
+xtc_profile = ARGV[1] || 'default'
+
+### prepare
+
+do_system(File.expand_path(File.join(File.dirname(__FILE__), 'ci-prepare.rb')))
+
+working_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'Briar'))
+
+Dir.chdir working_dir do
+
+  do_system('rm -rf calabash.framework')
+  do_system('bundle install')
+  do_system('bundle exec calabash-ios download')
+
+  File.open('.env', 'a') { |f| f.write("XTC_SERIES=\"toolchain-released\"") }
+
+  do_system("bundle exec briar xtc #{xtc_device_set} #{xtc_profile}")
+
+  # sometimes json 1.8.1 cannot be found
+  install_gem 'json'
+
+  do_system('./jenkins-calabash.sh')
+
+end
