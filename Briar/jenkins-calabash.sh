1d1a23294afe607a24e83dfcabd32d68a419a1f8
Joshua Moody <joshuajmoody@gmail.com>
2014-10-22 22:33:35 +0200

    Fix keychain access; respond to new Jenkins variables

--- Briar/jenkins-calabash.sh
+++ Briar/jenkins-calabash.sh
@@ -24,17 +24,7 @@ mkdir -p "${CAL_BUILD_DIR}"
 echo "INFO: unlocking the keychain"
 
 if [ "${USER}" = "jenkins" ]; then
-    xcrun security default-keychain -d user -s "${JENKINS_KEYCHAIN}"
-    RETVAL=$?
-    if [ ${RETVAL} != 0 ]; then
-        echo "FAIL: could not set the default keychain"
-        exit ${RETVAL}
-    fi
-fi
-
-# unlock the keychain - WARNING: might need to run 1x in UI to 'allow always'
-if [ "${USER}" = "jenkins" ]; then
-    xcrun security unlock-keychain -p "${JENKINS_KEYCHAIN_PASS}" "${JENKINS_KEYCHAIN}"
+    xcrun security unlock-keychain -p "${KEYCHAIN_PASSWORD}" "${KEYCHAIN_PATH}"
     RETVAL=$?
     if [ ${RETVAL} != 0 ]; then
         echo "FAIL: could not unlock the keychain"
