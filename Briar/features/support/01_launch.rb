69a230d7f6ce7c308e263fa67e8e19363d10a6db
Joshua Moody <joshuajmoody@gmail.com>
2014-12-02 18:38:08 +0100

    WIP Force :shared_element strategy
    Awaiting merge of PR #633
    
    Make shared element strategy default for ios8 #633
    https://github.com/calabash/calabash-ios/pull/633

--- Briar/features/support/01_launch.rb
+++ Briar/features/support/01_launch.rb
@@ -18,7 +18,11 @@ end
 #noinspection RubyUnusedLocalVariable
 Before do |scenario|
 
-  LaunchControl.launcher.relaunch
+  launch_options = {
+        :uia_strategy => :shared_element
+  }
+
+  LaunchControl.launcher.relaunch(launch_options)
   LaunchControl.launcher.calabash_notify(self)
 
   # I have tried launching 1x, but the results are not good on the simulators.
