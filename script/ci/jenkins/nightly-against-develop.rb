a2bbf9106422613c8d278b97694bb1f16d1d59a1
Joshua Moody <joshuajmoody@gmail.com>
2015-03-02 21:21:13 +0100

    More iOS 8, no iOS 6 in CI device sets

--- script/ci/jenkins/nightly-against-develop.rb
+++ script/ci/jenkins/nightly-against-develop.rb
@@ -6,7 +6,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'ci-helpers'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'run-develop'))
 
-xtc_device_set = ARGV[0] || '4af70b0b'
+xtc_device_set = ARGV[0] || '1cdb4285'
 xtc_profile = ARGV[1] || 'default'
 # accept 2 additional cucumber args for -t < tags >
 cucumber_args = ''
--- script/ci/jenkins/nightly-against-masters.rb
+++ script/ci/jenkins/nightly-against-masters.rb
@@ -3,7 +3,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'ci-helpers'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'run-masters'))
 
-xtc_device_set = ARGV[0] || '096dc09c'
+xtc_device_set = ARGV[0] || 'dd5c70c0'
 xtc_profile = ARGV[1] || 'default'
 
 
