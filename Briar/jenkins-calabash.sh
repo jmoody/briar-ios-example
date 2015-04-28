bda81f3fd63f4165f0a39628fd38b7e19d9b199f
Joshua Moody <joshuajmoody@gmail.com>
2015-04-28 23:28:32 +0200

    Update codesigning in sim build scripts

--- Briar/jenkins-calabash.sh
+++ Briar/jenkins-calabash.sh
@@ -30,14 +30,18 @@ if [ "${USER}" = "jenkins" ]; then
         echo "FAIL: could not unlock the keychain"
         exit ${RETVAL}
     fi
+
     xcrun security set-keychain-settings -t 3600 -l "${KEYCHAIN_PATH}"
     OTHER_CODE_SIGN_FLAGS="--keychain=${KEYCHAIN_PATH}"
+    xcrun security show-keychain-info ${KEYCHAIN_PATH}
+    xcrun security -v list-keychains -d user
 fi
 
 # build the -cal target to get it on the phone
 set +o errexit
 
 xcrun xcodebuild \
+    OTHER_CODE_SIGN_FLAGS=${OTHER_CODE_SIGN_FLAGS} \
     -SYMROOT="${CAL_BUILD_DIR}" \
     -derivedDataPath "${CAL_BUILD_DIR}" \
     -workspace "${XC_WORKSPACE}" \
