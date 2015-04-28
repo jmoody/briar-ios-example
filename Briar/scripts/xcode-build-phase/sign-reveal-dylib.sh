cab5729b18d8010a668b0616aec7f6e029f1c963
Joshua Moody <joshuajmoody@gmail.com>
2015-04-28 20:21:00 +0200

    WIP hunting for the magic code sign identity

--- Briar/scripts/xcode-build-phase/sign-reveal-dylib.sh
+++ Briar/scripts/xcode-build-phase/sign-reveal-dylib.sh
@@ -39,6 +39,7 @@ fi
 
 if [ -n "${CODE_SIGN_IDENTITY}" ]; then
   REVEAL_DYLIB_PATH="${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}/libReveal.dylib"
+  echo "code sign identity: ${CODE_SIGN_IDENTITY}"
   if [ "${USER}" = "jenkins" ]; then
     xcrun codesign -v --keychain ${KEYCHAIN_PATH} -fs ${CODE_SIGN_IDENTITY} "${REVEAL_DYLIB_PATH}"
   else
