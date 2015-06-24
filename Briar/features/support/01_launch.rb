e4546e50f210c00e1a40bdcaa58a23111015fc3a
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2015-06-24 16:56:03 -0700

    output profiling data after cucumber run

--- Briar/features/support/01_launch.rb
+++ Briar/features/support/01_launch.rb
@@ -14,6 +14,18 @@ module LaunchControl
   end
 end
 
+require 'ruby-prof'
+
+puts "BEFORE SUITE, starting profile"
+# Profile the code
+RubyProf.start
+
+at_exit do
+  result = RubyProf.stop
+
+  printer = RubyProf::MultiPrinter.new(result)
+  printer.print(:path => ".", :profile => "profile")
+end
 
 #noinspection RubyUnusedLocalVariable
 Before do |scenario|
